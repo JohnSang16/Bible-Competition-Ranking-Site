d9734e569fcd21d681deda62fa49b120c49f80f4
JohnSang16 <johnsang1970@gmail.com>
2026-02-19 01:39:16 -0500

    update login method in routes.py to handle 'POST' method for login submission, implement base.html to make flashing messages functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -8,8 +8,11 @@ def index():
      user = {'username': 'Jacob'}
      return render_template('main.html', title = 'main', user = user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
-
