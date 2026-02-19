ce813382710c5b5e78ae30fe9836f5aa10991c84
JohnSang16 <johnsang1970@gmail.com>
2026-02-19 01:19:20 -0500

    fix: routes.py by adding route('/'),add base.html link for login url only

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,14 @@ from app import app
 from flask import render_template
 from app.forms import LoginForm
 
+@app.route('/')
+@app.route('/index')
+def index():
+     user = {'username': 'Jacob'}
+     return render_template('main.html', title = 'main', user = user)
 
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form = form)
-# @app.route('/index')
-# def index():
-#     user = {'username': 'Jacob'}
-#     return render_template('main.html', title = 'main', user = user)
+    return render_template('login.html', title='Sign In', form=form)
+
